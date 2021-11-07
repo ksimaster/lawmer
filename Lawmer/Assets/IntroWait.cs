7d2242e98d88ae4c4fe0fbb9ee073800a1870d3d
IIEC-kycaka <34373675+IIEC-kycaka@users.noreply.github.com>
2021-11-07 18:21:57 +0300

    видео добавлено

---
+++ Lawmer/Assets/IntroWait.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroWait : MonoBehaviour
+{
+
+    public int waitTime;
+
+    void Start()
+    {
+        StartCoroutine(WaitForLevel());
+    }
+
+    IEnumerator WaitForLevel()
+    {
+        yield return new WaitForSeconds(waitTime);
+        SceneManager.LoadScene(1);
+
+    }
+
+}
